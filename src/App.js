28345ba6815e1670a66718204902ea9b6f9c51cd
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-07-16 09:43:47 -0500

    Updated with particles module.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { Component } from 'react';
+// import Particles from 'react-particles-js';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
@@ -5,16 +7,33 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import 'tachyons';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/*<FaceRecognition /> */}
-    </div>
-  );
+// const particlesOptions = {
+//   particles: {
+//     number: {
+//       value: 30,
+//       density: {
+//         enable: true,
+//         value_area: 800
+//       }
+//     }
+//   }
+// }
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        {/* <Particles className='particles'
+          params={particlesOptions}
+        /> */}
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {/*<FaceRecognition /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
